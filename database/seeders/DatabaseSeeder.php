17dbfda3935dffd49c9111994350b32b475cd1eb
raafi <raafistudent@gmail.com>
2026-04-23 14:48:06 +0700

    feat: membuat akun admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,22 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Membuat akun Admin khusus Petugas BPBD
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Petugas BPBD Tegal',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password123'), // Ini passwordnya
         ]);
     }
 }
